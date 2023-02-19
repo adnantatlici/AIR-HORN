7861a012d5a4b4e23d1cae44b708bfd92fdfcde9
Mustafa Adnan Tatlici <109048001+adnantatlici@users.noreply.github.com>
2023-02-19 17:37:00 +0300

    Add files via upload

---
+++ AirHorne/AirHorne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirHorne
+//
+//  Created by Mustafa Adnan Tatlıcı on 19.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        
+        
+    }
+
+    
+    
+
+}
+
